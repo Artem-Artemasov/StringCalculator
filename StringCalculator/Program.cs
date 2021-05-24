0e841069951b352cf2a447a188d2e678b342789d
Artem-Artemasov <artem.artemasov@ukad-group.com>
2021-05-24 12:58:54 +0300

    Add project files.

---
+++ StringCalculator/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StringCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var calculator = new StringCalculator();
+            calculator.Add("");
+        }
+    }
+}
---
+++ StringCalculator/StringCalculator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator
+{
+    public class StringCalculator
+    {
+        private List<int> ToIntList(string numbers)
+        {
+          List<int> numbers_int = new List<int>() { 0 };
+          int listIndex = 0;
+
+            foreach (char simpleNumber in numbers)
+            {
+                //Следующее число
+                if (simpleNumber == ',')
+                {
+                    numbers_int.Add(0);
+                    listIndex++;
+                } 
+                //Если число
+                if (simpleNumber >= 48 && simpleNumber <= 57)
+                {
+                    numbers_int[listIndex] *= 10;
+                    numbers_int[listIndex] += simpleNumber - 48;
+                }
+            }
+          return numbers_int;
+        }
+
+        public int Add(string numbers)
+        {
+            int sum = 0;
+            var numbers_int = ToIntList(numbers);
+
+            foreach(var number in numbers_int)
+            {
+                sum += number;
+            }
+
+            return sum;
+        }
+    }
+}
